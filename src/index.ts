f7307b20765bb37abd86955d46b1dd3db9198088
Alexis Sniffer <alexis.sniffer@gmail.com>
2026-02-21 02:09:21 -0500

    configuracion de servidor websocket

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,33 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: any }) {
+    const httpServer = strapi.server.httpServer;
+    if (!httpServer) {
+      strapi.log.error("Could not obtain the HTTP server for socket.io");
+      return;
+    }
+
+    const { Server } = await import("socket.io");
+    const io = new Server(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    io.on("connection", (socket) => {
+      strapi.log.info("Client connected to socket.io");
+
+      socket.on("message", (data) => {
+        strapi.log.info(`Coordinates received: ${JSON.stringify(data)}`);
+      });
+
+      socket.on("disconnect", () => {
+        strapi.log.info("Client disconnected from socket.io");
+      });
+    });
+
+    strapi.io = io;
+  },
 };
